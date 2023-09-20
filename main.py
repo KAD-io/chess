40bb3160d551161f992f7d18cc067fb1593cf4d3
Andrew <kad.io.work@gmail.com>
2023-09-20 19:10:54 +0300

    code review: removed redundant methods

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ class Player:
         self.name = name
         self.color = color
 
-    def get_name(self):
-        return self.name
-
-    def get_color(self):
-        return self.color
-
     @property
     def is_having_king(self):
         return self.is_king_taken
@@ -24,15 +18,15 @@ class Player:
         coord2 = ''
         is_valid_move_player = False
         while not is_valid_move_player:
-            print(f'move: {self.get_name()} ({self.get_color()})')
+            print(f'move: {self.name} ({self.color})')
             input_coord1 = input('input coordinate 1: ').lower()
             input_coord2 = input('input coordinate 2: ').lower()
-            is_valid_move_player = chessboard.is_valid_move_player(self.get_color(), input_coord1, input_coord2)
+            is_valid_move_player = chessboard.is_valid_move_player(self.color, input_coord1, input_coord2)
             if not is_valid_move_player:
                 print("invalid input of coordinates")
-            else:
-                coord1 = input_coord1
-                coord2 = input_coord2
+                continue
+            coord1 = input_coord1
+            coord2 = input_coord2
         self.is_king_taken = chessboard.move_player(coord1, coord2)
         return self.is_king_taken
 
@@ -52,7 +46,7 @@ def play():
 
     winner = player1 if player1.is_king_taken else player2
 
-    print(f'Winner {winner.get_name()} ({winner.get_color()})')
+    print(f'Winner {winner.name} ({winner.color})')
 
 
 if __name__ == '__main__':
